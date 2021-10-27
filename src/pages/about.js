dfb25a89e23c400cc39392aec10c4619dc8e1c3d
Miguel Teixeira <64308962+migueljsteixeira@users.noreply.github.com>
2021-10-27 23:33:22 +0100

    Creating links

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,9 +1,11 @@
 import * as React from 'react';
+import { Link } from 'gatsby';
 
 const AboutPage = () => {
   return (
     <main>
       <h1>About this website</h1>
+      <Link to="/">back to home</Link>
     </main>
   );
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
 import * as React from 'react';
+import { Link } from 'gatsby';
 
 const IndexPage = () => {
   return (
     <main>
       <h1>Hello this is a test</h1>
+      <Link to="/about">about</Link>
     </main>
   );
 };
