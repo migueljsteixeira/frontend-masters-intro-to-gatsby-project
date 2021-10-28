2b2bf50bd357d86182d339432de3519e593bc34b
Miguel Teixeira <64308962+migueljsteixeira@users.noreply.github.com>
2021-10-28 21:48:29 +0100

    Adding scoped styles with CSS Modules

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { Link, useStaticQuery, graphql } from 'gatsby';
 import { Seo } from './seo.js';
 import '../styles/global.css';
+import { header, content } from '../styles/layout.module.css';
 
 export default function Layout({
   children,
@@ -25,13 +26,13 @@ export default function Layout({
   return (
     <>
       <Seo title={title} description={description} image={image} path={path} />
-      <header>
+      <header className={header}>
         <Link to="/">{meta.title}</Link>
         <nav>
           <Link to="/about">About</Link>
         </nav>
       </header>
-      <main>{children}</main>
+      <main className={content}>{children}</main>
     </>
   );
 }
