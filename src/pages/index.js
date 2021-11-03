a07ed1244db709c03ff6ec3208f96118187e4ac0
Miguel Teixeira <64308962+migueljsteixeira@users.noreply.github.com>
2021-11-03 19:26:28 +0000

    Show recent MDX posts

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,42 @@
 import * as React from 'react';
 import { Link, useStaticQuery, graphql } from 'gatsby';
 import Layout from '../components/layout.js';
+import PostLayout from '../components/post-layout.js';
 
 const IndexPage = () => {
+  const data = useStaticQuery(graphql`
+    query GetBlogPosts {
+      allMdx(sort: { fields: frontmatter___date, order: DESC }) {
+        nodes {
+          id
+          slug
+          frontmatter {
+            title
+            date(fromNow: true)
+            Desciption
+          }
+        }
+      }
+    }
+  `);
+
+  const posts = data.allMdx.nodes;
+
   return (
     <Layout>
       <h1>Hello this is a test</h1>
       <Link to="/about">about</Link>
+
+      <h2>Checkout my recent blog posts</h2>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>
+            <Link to={post.slug}>{post.frontmatter.title}</Link>
+            {'  '}
+            <small>posted {post.frontmatter.date}</small>
+          </li>
+        ))}
+      </ul>
     </Layout>
   );
 };
