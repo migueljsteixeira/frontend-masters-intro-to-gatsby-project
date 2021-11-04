783288e5bb6786dae204de6962e7ff750c1beeb2
Miguel Teixeira <64308962+migueljsteixeira@users.noreply.github.com>
2021-11-04 09:16:24 +0000

    Creating Pages from Thirdparty data

---
+++ src/pages/episode/{SanityEpisode.slug__current}.js
@@ -0,0 +1,52 @@
+import * as React from 'react';
+import { graphql } from 'gatsby';
+import { GatsbyImage } from 'gatsby-plugin-image';
+import Layout from '../../components/layout.js';
+
+export const query = graphql`
+  query SanityEpisode($id: String!) {
+    sanityEpisode(id: { eq: $id }) {
+      title
+      description
+      slug {
+        current
+      }
+      youtubeID
+      date(fromNow: true)
+      image {
+        asset {
+          gatsbyImageData(placeholder: DOMINANT_COLOR)
+        }
+      }
+    }
+  }
+`;
+
+export default function SanityEpisode({ data }) {
+  const episode = data.sanityEpisode;
+
+  return (
+    <Layout title={episode.title} description={episode.description}>
+      <GatsbyImage
+        image={episode.image.asset.gatsbyImageData}
+        alt={episode.title}
+      />
+      <h1>{episode.title}</h1>
+      <p>
+        (posted {episode.date}) - {episode.description}
+      </p>
+      <ul>
+        <li>
+          <a href={`https://www.learnwithjason.dev/${episode.slug.current}`}>
+            full episode details
+          </a>
+        </li>
+        <li>
+          <a href={`https://www.youtube.com/watch?v=${episode.youtubeID}`}>
+            watch on youtube
+          </a>
+        </li>
+      </ul>
+    </Layout>
+  );
+}
